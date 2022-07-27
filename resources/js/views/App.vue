7ba83006a40b0fec30fb98e3dcf09f3e4a88b871
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-07-27 15:41:00 +0200

    eseguita chiamata axios per prendere i dati dalla api

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,15 +1,40 @@
 <template>
 <div>
     <h1>
-        VUE JS
+        Blog di ScarpaPress
     </h1>
+    <ul>
+        <li v-for="post in posts" :key="post.slug">
+            {{post.title}}
+        </li>
+    </ul>
 </div>
   
 </template>
 
 <script>
+
+
+
 export default {
-    name: 'App'
+    name: 'App',
+    data(){
+        return {
+            posts: []
+        }
+    },
+
+    created(){
+        axios.get('http://127.0.0.1:8000/api/posts')
+        .then((response) => {
+            console.log(response.data);
+            this.posts = response.data;
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
 }
 </script>
 
