c9a3ca03ea4a15643d52bbaef14c18d00dd4bd41
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-07-27 16:40:55 +0200

    aggiunto componente card e passato dati del post tramite props

---
+++ resources/js/components/commons/BaseCard.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="card-post">
+    <h3>{{title}}</h3>
+    <div class="card-post-content">
+       {{content}}
+    </div>
+    <a href="">Visualizza il post</a>
+
+</div>
+  
+</template>
+
+<script>
+export default {
+    name:'BaseCard',
+    props:{
+        title:{
+            type: String
+        },
+        content:{
+            type: String
+        },
+
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.card-post{
+    background-color: white;
+    border-radius: 0.9375rem;
+    padding: 0.625rem;
+    box-shadow: 0 0 5px 2px #ddd;
+}
+
+</style>
